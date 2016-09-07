7f6248f90672c17532c862937fe9c87e42605c93
Samuels <dsamuel@acbc32cc2bf1.ant.amazon.com>
2016-09-07 15:37:49 +0900

    new update

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Start app
+export AWS_ACCESS_KEY_ID="ASIAJQEIADK2ELVT7EZA"
+export AWS_SECRET_ACCESS_KEY="Zuj2LfnjNgV5/IuNlLcqLl13+kBPrGBLbjN0Hfx0"
+export AWS_SESSION_TOKEN="FQoDYXdzEOj//////////wEaDFfOuVskqvIiNQwmsiLZAaFtsE0BITjX38jLf2FTUXfVO1307Qq//xa/8Ok1XzfWaTdtePpTLfRhYNx7eeb1P7Itw+myhHAhkjuSB2OU2I/5GVGyhPDG0sJ1Z1YNRILjeeg3gYgFO7TKsxPsbbWQ8xspE+zIhaHOVmtVVjY6YCcS9yH32tluLLhrpyH8s1N98erSuMVpeRFsLScMsqdrEFrLJ0AqKTyeZeq77ep7Ne1Pxc1w7DDZ1Aa+oP5wyPYdv+25z4LG+t38OjWHDzUpE9NyEd9bWlxBJ4lGmBR1E6XJGsQ9XDXowmwo3+m+vgU="
+    
+export us=ba1b1015be
+python ~/unicorn/server.py 0 $us JQMwW http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 1 $us 55JR4 http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 2 $us luG0W http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 3 $us PTtsI http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 4 $us WEDYx http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 5 $us tpfI0 http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 6 $us SrdPE http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 7 $us hI8fj http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 8 $us hGBfp http://apac-tech-summit.unicorn.rentals/score &
+python ~/unicorn/server.py 9 $us kuunL http://apac-tech-summit.unicorn.rentals/score &
